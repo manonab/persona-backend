b46ad4690e7f2b9440348b6afd91950f9a30e1b6
Manon <45658243+manonab@users.noreply.github.com>
2025-02-13 10:59:17 +0100

    added enabled cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
