4a3a4e298e5deeb34fe0a3bf9028ea8a5f200ab0
Manon <45658243+manonab@users.noreply.github.com>
2025-02-13 14:24:44 +0100

    added new url to fix cors policy

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://persona-frontend-kvpesq63q-manons-projects-def0a134.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
